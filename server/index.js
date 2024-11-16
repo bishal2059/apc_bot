9c5cc33920d9c9f587eb6778c9736cf3418a29fd
bishal2059 <bishal.sap21@gmail.com>
2024-11-16 23:56:02 +0545

    setup

--- server/index.js
+++ server/index.js
@@ -1,9 +1,12 @@
+import http from 'http';
 import express from 'express';
 
 const PORT = 8080;
 
 const app = express();
 
-app.listen(PORT,()=>{
-    console.log(`Server listening to port localhost:${8080}`)
+const server = http.createServer(app);
+
+server.listen(PORT,()=>{
+    console.log(`Server listening to port http://localhost:${8080}`)
 })
