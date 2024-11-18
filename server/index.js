c7fdad7aaa79e83d49e58ad0866743499125c82b
bishal2059 <bishal.sap21@gmail.com>
2024-11-18 23:55:47 +0545

    api started

--- server/index.js
+++ server/index.js
@@ -1,12 +1,26 @@
-import http from 'http';
-import express from 'express';
+import http from "http";
+import express from "express";
+import path from "path";
 
 const PORT = 8080;
 
 const app = express();
 
+app.use(express.static(path.join(__dirname, "..", "client", "dist")));
+
+const rootController = function (req, res) {
+  res
+    .statusCode(200)
+    .sendFile(path.join(__dirname, "..", "client", "index.html"));
+  return;
+};
+
+app.use("/", rootController);
+
+app.use("*", rootController);
+
 const server = http.createServer(app);
 
-server.listen(PORT,()=>{
-    console.log(`Server listening to port http://localhost:${8080}`)
-})
+server.listen(PORT, () => {
+  console.log(`Server listening to port http://localhost:${8080}`);
+});
