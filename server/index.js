f4ddba31c86850a3b39198efe0290b093839cdb6
bishal2059 <bishal.sap21@gmail.com>
2024-11-19 00:33:45 +0545

    dev-prod added

--- server/index.js
+++ server/index.js
@@ -1,25 +1,43 @@
 import http from "http";
+import { configDotenv } from "dotenv";
 import express from "express";
 import path from "path";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import cors from "cors";
 
-const PORT = 8080;
+const __filename = fileURLToPath(import.meta.url);
+
+const __dirname = dirname(__filename);
+
+configDotenv();
+
+const mode = process.env.NODE_PROCESS || "development";
+
+const PORT = process.env.PORT;
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, "..", "client", "dist")));
+if (mode == "production") {
+  app.use(express.static(path.join(__dirname, "..", "client", "dist")));
 
-const rootController = function (req, res) {
-  res
-    .statusCode(200)
-    .sendFile(path.join(__dirname, "..", "client", "index.html"));
-  return;
-};
+  const rootController = function (req, res) {
+    res
+      .status(200)
+      .sendFile(path.join(__dirname, "..", "client", "index.html"));
+    return;
+  };
 
-app.use("/", rootController);
+  app.use("/", rootController);
 
-app.use("*", rootController);
+  app.use("*", rootController);
+} else {
+  app.use(cors());
+}
 
 const server = http.createServer(app);
+
+console.log(mode);
 
 server.listen(PORT, () => {
   console.log(`Server listening to port http://localhost:${8080}`);
