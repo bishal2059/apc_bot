bcad45dcc1be00880dbeb6a6c6c4c937669e6517
bishal2059 <bishal.sap21@gmail.com>
2024-11-23 23:37:54 +0545

    bug fixes

--- server/src/controller/login.controller.js
+++ server/src/controller/login.controller.js
@@ -1,5 +1,9 @@
 const loginController = function (req, res) {
-  res.status(200).send(`<a href="/auth/google"> Sign in with google</a>`);
+  res
+    .status(200)
+    .send(
+      `<html><head><title>Log in </title></head><body><a href="/auth/google"> Sign in with google</a></body></html>`
+    );
 };
 
 export default loginController;
